f51c8537bb96fc7a168fbb64473f265c69d9ae5c
Madhav Solanki <madhav09solanki@gmail.com>
2024-12-19 20:05:03 +0530

    save-get Notes implemented

--- src/main/java/com/notes/service/NotesService.java
+++ src/main/java/com/notes/service/NotesService.java
@@ -3,10 +3,11 @@ package com.notes.service;
 import java.util.List;
 
 import com.notes.dto.NotesDto;
+import com.notes.exception.ResourceNotFoundException;
 
 public interface NotesService {
   
-  public Boolean saveNotes(NotesDto notesDto);
+  public Boolean saveNotes(NotesDto notesDto) throws ResourceNotFoundException, Exception;
 
   public List<NotesDto> getAllNotes();
 
